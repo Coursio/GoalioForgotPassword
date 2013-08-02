e8c78819a930263fa93f6449fd8fe01277e1768d
Vladimir Osipov <vladimir.osipov@coursio.com>
2013-08-02 09:53:24 +0200

    Coursio/App#667 user check message for password recovery

--- src/GoalioForgotPassword/Controller/ForgotController.php
+++ src/GoalioForgotPassword/Controller/ForgotController.php
@@ -63,16 +63,14 @@ class ForgotController extends AbstractActionController
 
     public function forgotAction()
     {
+        $form    = $this->getForgotForm();
         $service = $this->getPasswordService();
         $service->cleanExpiredForgotRequests();
 
-        $request = $this->getRequest();
-        $form    = $this->getForgotForm();
-
-        if ( $this->getRequest()->isPost() )
+        if ($this->getRequest()->isPost())
         {
             $form->setData($this->getRequest()->getPost());
-            if ( $form->isValid() )
+            if ($form->isValid())
             {
                 $userService = $this->getUserService();
 
@@ -80,63 +78,74 @@ class ForgotController extends AbstractActionController
                 $user = $userService->getUserMapper()->findByEmail($email);
 
                 //only send request when email is found
-                if($user != null) {
+                if ($user != null)
+                {
                     $service->sendProcessForgotRequest($user->getId(), $email);
                 }
 
-                $vm = new ViewModel(array('email' => $email));
+                $vm = new ViewModel(array
+                (
+                    'email'   => $email,
+                    'message' => $user ? null : 'Email not found',
+                ));
                 $vm->setTemplate('goalio-forgot-password/forgot/sent');
                 return $vm;
-            } else {
+            }
+            else
+            {
                 $this->flashMessenger()->setNamespace('goalioforgotpassword-forgot-form')->addMessage($this->failedMessage);
-                return array(
+                return array
+                (
                     'forgotForm' => $form,
                 );
             }
         }
 
         // Render the form
-        return array(
+        return array
+        (
             'forgotForm' => $form,
         );
     }
 
     public function resetAction()
     {
+        $form    = $this->getResetForm();
         $service = $this->getPasswordService();
         $service->cleanExpiredForgotRequests();
 
-        $request = $this->getRequest();
-        $form    = $this->getResetForm();
-
-        $userId    = $this->params()->fromRoute('userId', null);
-        $token     = $this->params()->fromRoute('token', null);
-
+        $userId   = $this->params()->fromRoute('userId', null);
+        $token    = $this->params()->fromRoute('token', null);
         $password = $service->getPasswordMapper()->findByUserIdRequestKey($userId, $token);
 
         //no request for a new password found
-        if($password === null || $password == false) {
+        if($password === null || $password == false)
+        {
             return $this->redirect()->toRoute('zfcuser/forgotpassword');
         }
 
         $userService = $this->getUserService();
         $user = $userService->getUserMapper()->findById($userId);
 
-        if ( $this->getRequest()->isPost() )
+        if ($this->getRequest()->isPost())
         {
             $form->setData($this->getRequest()->getPost());
-            if ( $form->isValid() && $user !== null )
+            if ($form->isValid() && $user !== null)
             {
                 $service->resetPassword($password, $user, $form->getData());
 
-                $vm = new ViewModel(array('email' => $user->getEmail()));
+                $vm = new ViewModel(array
+                (
+                    'email' => $user->getEmail(),
+                ));
                 $vm->setTemplate('goalio-forgot-password/forgot/passwordchanged');
                 return $vm;
             }
         }
 
         // Render the form
-        return array(
+        return array
+        (
             'resetForm' => $form,
             'userId'    => $userId,
             'token'     => $token,
@@ -145,58 +154,84 @@ class ForgotController extends AbstractActionController
     }
 
     /**
-     * Getters/setters for DI stuff
+     * @return array|UserService|object
      */
-
     public function getUserService()
     {
-        if (!$this->userService) {
+        if (!$this->userService)
+        {
             $this->userService = $this->getServiceLocator()->get('zfcuser_user_service');
         }
         return $this->userService;
     }
 
+    /**
+     * @param UserService $userService
+     * @return $this
+     */
     public function setUserService(UserService $userService)
     {
         $this->userService = $userService;
         return $this;
     }
 
+    /**
+     * @return array|PasswordService|object
+     */
     public function getPasswordService()
     {
-        if (!$this->passwordService) {
+        if (!$this->passwordService)
+        {
             $this->passwordService = $this->getServiceLocator()->get('goalioforgotpassword_password_service');
         }
         return $this->passwordService;
     }
 
+    /**
+     * @param PasswordService $passwordService
+     * @return $this
+     */
     public function setPasswordService(PasswordService $passwordService)
     {
         $this->passwordService = $passwordService;
         return $this;
     }
 
+    /**
+     * @return Form
+     */
     public function getForgotForm()
     {
-        if (!$this->forgotForm) {
+        if (!$this->forgotForm)
+        {
             $this->setForgotForm($this->getServiceLocator()->get('goalioforgotpassword_forgot_form'));
         }
         return $this->forgotForm;
     }
 
+    /**
+     * @param Form $forgotForm
+     */
     public function setForgotForm(Form $forgotForm)
     {
         $this->forgotForm = $forgotForm;
     }
 
+    /**
+     * @return Form
+     */
     public function getResetForm()
     {
-        if (!$this->resetForm) {
+        if (!$this->resetForm)
+        {
             $this->setResetForm($this->getServiceLocator()->get('goalioforgotpassword_reset_form'));
         }
         return $this->resetForm;
     }
 
+    /**
+     * @param Form $resetForm
+     */
     public function setResetForm(Form $resetForm)
     {
         $this->resetForm = $resetForm;
@@ -221,7 +256,8 @@ class ForgotController extends AbstractActionController
      */
     public function getOptions()
     {
-        if (!$this->options instanceof ForgotControllerOptionsInterface) {
+        if (!$this->options instanceof ForgotControllerOptionsInterface)
+        {
             $this->setOptions($this->getServiceLocator()->get('goalioforgotpassword_module_options'));
         }
         return $this->options;
